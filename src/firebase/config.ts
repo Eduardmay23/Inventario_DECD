f81775d21ccc9730421e20a71149c483d25d4bf0
Eduard May <eduardmay537@gmail.com>
2025-10-06 17:50:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8280891146-14a6a",
+  "appId": "1:47403397589:web:fee82218e1cc97c4ef4d5c",
+  "apiKey": "AIzaSyCsVGmWR6dStX0mFzZnLzUvZaBr0MTpByE",
+  "authDomain": "studio-8280891146-14a6a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "47403397589"
+};
