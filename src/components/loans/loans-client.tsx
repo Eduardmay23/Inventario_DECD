befd31b18bd079b5c86562544b57894aae62237e
Eduard May <eduardmay537@gmail.com>
2025-10-07 01:26:32 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/loans/loans-client.tsx
+++ src/components/loans/loans-client.tsx
@@ -220,7 +220,7 @@ export default function LoansClient({ loans, products }: LoansClientProps) {
             <AlertDialogTitle>¿Estás absolutamente seguro?</AlertDialogTitle>
             <AlertDialogDescription>
               Esta acción no se puede deshacer. Esto eliminará permanentemente el préstamo del producto "{loanToDelete?.productName}".
-            </description>
+            </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogCancel>Cancelar</AlertDialogCancel>
