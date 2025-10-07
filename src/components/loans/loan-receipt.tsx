56a6d80bf83bdf479a9e3db458466daf1d13f547
Eduard May <eduardmay537@gmail.com>
2025-10-07 03:12:25 +0000

    noooo, era invertir de posicion "entregado por, y el editable"

--- src/components/loans/loan-receipt.tsx
+++ src/components/loans/loan-receipt.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import type { Loan } from '@/lib/types';
 import { Button } from '@/components/ui/button';
 import { format } from 'date-fns';
